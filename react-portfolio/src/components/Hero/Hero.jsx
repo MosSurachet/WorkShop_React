7b088dacc0616c2466b2e79716d8e3241dd2ebe8
Surachet Intamongkol <diymos78@gmail.com>
2024-02-03 18:20:22 +0700

    Fix Hero Module CSS

--- react-portfolio/src/components/Hero/Hero.jsx
+++ react-portfolio/src/components/Hero/Hero.jsx
@@ -7,20 +7,19 @@ function Hero() {
       <div className={styles.contianer}>
         <div className= {styles.hero_con}>
           <div className={styles.hero_info}>
-                <p className= {styles.text_1}>Hi, it’s me</p>
-                <h3 className= {styles.text_2}>John Doe</h3>
-                <p className = {styles.text_3}>I’m a Front-end Developer</p>
-                <p className = {styles.text_4}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga fugiat commodi <br />quas cupiditate minus dolore nulla eos accusantium labore explicabo.</p>
-                <ul className = {styles.hero_social}>
-                    <li><a href="#"><FaGithub/></a></li>
-                    <li><a href="#"><FaLinkedinIn/></a></li>
-                    <li><a href="#"><FaFacebookF/></a></li>
-                </ul>
+              <p className = {styles.text_1}>Hi, it’s me</p>
+              <h3 className = {styles.text_2}>John Doe</h3>
+              <p className = {styles.text_3}>
+                I’m a Front-end Developer</p>
+              <p className = {styles.text_4}>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga fugiat commodi <br />quas cupiditate minus dolore nulla eos accusantium labore explicabo.</p>
+              <ul className = {styles.hero_social}>
+                  <li><a href="#"> <FaGithub /> </a></li>
+                  <li><a href="#"> <FaLinkedinIn /></a></li>
+                  <li><a href="#"><FaFacebookF /></a></li>
+              </ul>
             </div>
-            <div>
-              <div className = {styles.hero_img}>
-            </div>
-          </div>
+            <div><div className = {styles.hero_img}></div></div>
         </div>
       </div>
     </div>

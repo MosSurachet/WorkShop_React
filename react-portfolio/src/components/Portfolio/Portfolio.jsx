22b9c280f0bcdb56a00abddfedf3c1f31201b9f8
Surachet Intamongkol <diymos78@gmail.com>
2024-02-03 22:04:18 +0700

    Add files Portfolio

--- react-portfolio/src/components/Portfolio/Portfolio.jsx
+++ react-portfolio/src/components/Portfolio/Portfolio.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import styles from './Portfolio.module.css'
 
-function Portfolio() {
+function portfolio() {
   return (
-    <div>
-      
+    <div className={styles.port_con}>
+      <h3 className={styles.port_title}>Portfolio</h3>
+      <div className={styles.port_list}>
+        <div className={styles.port_items}>
+            <img src="https://plus.unsplash.com/premium_photo-1678565202049-9e37b2da8060?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8d2ViJTIwZGV2ZWxvcG1lbnR8ZW58MHx8MHx8fDA%3D" alt="" />
+            <p>Project Title</p>
+        </div>
+        <div className={styles.port_items}>
+            <img src="https://plus.unsplash.com/premium_photo-1678565202049-9e37b2da8060?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8d2ViJTIwZGV2ZWxvcG1lbnR8ZW58MHx8MHx8fDA%3D" alt="" />
+            <p>Project Title</p>
+        </div>
+        <div className={styles.port_items}>
+            <img src="https://plus.unsplash.com/premium_photo-1678565202049-9e37b2da8060?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8d2ViJTIwZGV2ZWxvcG1lbnR8ZW58MHx8MHx8fDA%3D" alt="" />
+            <p>Project Title</p>
+        </div>
+        <div className={styles.port_items}>
+            <img src="https://plus.unsplash.com/premium_photo-1678565202049-9e37b2da8060?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8d2ViJTIwZGV2ZWxvcG1lbnR8ZW58MHx8MHx8fDA%3D" alt="" />
+            <p>Project Title</p>
+        </div>
+        <div className={styles.port_items}>
+            <img src="https://plus.unsplash.com/premium_photo-1678565202049-9e37b2da8060?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8d2ViJTIwZGV2ZWxvcG1lbnR8ZW58MHx8MHx8fDA%3D" alt="" />
+            <p>Project Title</p>
+        </div>
+        <div className={styles.port_items}>
+            <img src="https://plus.unsplash.com/premium_photo-1678565202049-9e37b2da8060?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8d2ViJTIwZGV2ZWxvcG1lbnR8ZW58MHx8MHx8fDA%3D" alt="" />
+            <p>Project Title</p>
+        </div>
+      </div>
     </div>
   )
 }

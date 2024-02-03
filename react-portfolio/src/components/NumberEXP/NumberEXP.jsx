dad390eeda0c81435e4e9f28ebf2438ef9eff49f
Surachet Intamongkol <diymos78@gmail.com>
2024-02-03 23:20:37 +0700

    Add features NumberEXP

--- react-portfolio/src/components/NumberEXP/NumberEXP.jsx
+++ react-portfolio/src/components/NumberEXP/NumberEXP.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import styles from './NumberEXP.module.css'
 
 function NumberEXP() {
   return (
-    <div>
-      
+    <div className={styles.number_con}>
+      <div className={styles.number_items}>
+        <h3>100+</h3>
+        <p>Project Delivered</p>
+      </div>
+      <div className={styles.number_items}>
+        <h3>50+</h3>
+        <p>Companies Helped </p>
+      </div>
+      <div className={styles.number_items}>
+        <h3>10+</h3>
+        <p>Years of experience</p>
+      </div>
+      <div className={styles.number_items}>
+        <h3>200+</h3>
+        <p>Happy Clients</p>
+      </div>
     </div>
   )
 }

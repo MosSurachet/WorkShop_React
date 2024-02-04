a9ec3b9f2e0c082953596a9e4ebf94d9f25c8b4a
Surachet Intamongkol <diymos78@gmail.com>
2024-02-04 14:20:31 +0700

    innitialize Responsive disign website

--- react-portfolio/src/components/Navbar/Navbar.jsx
+++ react-portfolio/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styles from './Navbar.module.css'
+import { FaBars } from 'react-icons/fa6'
+
 function Navbar() {
+
+  const [isToggled, setToggle] = useState(false);
+
+  function handleToggle(state) {
+    setToggle(!isToggled) // f+f = true
+  }
+
   return (
     <nav>
         <div className={styles.container}>
@@ -19,6 +28,22 @@ function Navbar() {
                     <a href="#">Hire Me</a>
                 </div>
             </div>
+
+            {/* {Mobile Menu} */}
+
+            <FaBars className={styles.bars} onClick={handleToggle}/>
+            {isToggled ? (
+                <>
+                <ul className={styles.mobile_menu}>
+                    <li><a href="#">Skills</a></li>
+                    <li><a href="#">Portfolio</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+                <div className={styles.mobile_button}>
+                    <a href="#">Hire Me</a>
+                </div>
+                </>
+            ) : null }
         </div>
     </nav>
     

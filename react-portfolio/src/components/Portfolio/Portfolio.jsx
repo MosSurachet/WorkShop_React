03851657cbfac43338c0a636c96d977b874f5e91
Surachet Intamongkol <diymos78@gmail.com>
2024-02-05 11:27:34 +0700

    Done all animation

--- react-portfolio/src/components/Portfolio/Portfolio.jsx
+++ react-portfolio/src/components/Portfolio/Portfolio.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from './Portfolio.module.css'
+import Tilt from 'react-parallax-tilt';
 
 function portfolio() {
   return (
@@ -7,28 +8,40 @@ function portfolio() {
       <h3 className={styles.port_title}>Portfolio</h3>
       <div className={styles.port_list}>
         <div className={styles.port_items}>
+          <Tilt scale={1.1} transitionSpeed={2500} tiltReverse={true}>
             <img src="https://plus.unsplash.com/premium_photo-1678565202049-9e37b2da8060?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8d2ViJTIwZGV2ZWxvcG1lbnR8ZW58MHx8MHx8fDA%3D" alt="" />
-            <p>Project Title</p>
+          </Tilt>
+          <p>Project Title</p>
         </div>
         <div className={styles.port_items}>
+          <Tilt scale={1.1} transitionSpeed={2500} tiltReverse={true}>
             <img src="https://plus.unsplash.com/premium_photo-1678565202049-9e37b2da8060?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8d2ViJTIwZGV2ZWxvcG1lbnR8ZW58MHx8MHx8fDA%3D" alt="" />
-            <p>Project Title</p>
+          </Tilt>            
+          <p>Project Title</p>
         </div>
         <div className={styles.port_items}>
+         <Tilt scale={1.1} transitionSpeed={2500} tiltReverse={true}>
             <img src="https://plus.unsplash.com/premium_photo-1678565202049-9e37b2da8060?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8d2ViJTIwZGV2ZWxvcG1lbnR8ZW58MHx8MHx8fDA%3D" alt="" />
-            <p>Project Title</p>
+          </Tilt>            
+          <p>Project Title</p>
         </div>
         <div className={styles.port_items}>
+          <Tilt scale={1.1} transitionSpeed={2500} tiltReverse={true}>
             <img src="https://plus.unsplash.com/premium_photo-1678565202049-9e37b2da8060?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8d2ViJTIwZGV2ZWxvcG1lbnR8ZW58MHx8MHx8fDA%3D" alt="" />
-            <p>Project Title</p>
+          </Tilt>            
+          <p>Project Title</p>
         </div>
         <div className={styles.port_items}>
+          <Tilt scale={1.1} transitionSpeed={2500} tiltReverse={true}>
             <img src="https://plus.unsplash.com/premium_photo-1678565202049-9e37b2da8060?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8d2ViJTIwZGV2ZWxvcG1lbnR8ZW58MHx8MHx8fDA%3D" alt="" />
-            <p>Project Title</p>
+          </Tilt>            
+          <p>Project Title</p>
         </div>
         <div className={styles.port_items}>
+          <Tilt scale={1.1} transitionSpeed={2500} tiltReverse={true}>
             <img src="https://plus.unsplash.com/premium_photo-1678565202049-9e37b2da8060?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8d2ViJTIwZGV2ZWxvcG1lbnR8ZW58MHx8MHx8fDA%3D" alt="" />
-            <p>Project Title</p>
+          </Tilt>            
+          <p>Project Title</p>
         </div>
       </div>
     </div>
